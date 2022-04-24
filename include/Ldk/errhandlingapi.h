2a0884c67c204f3243e31b518a0ee564d8603b40
Jung-Kwnag Lee <ntoskrnl7@gmail.com>
2022-04-24 22:31:24 +0900

    RaiseException

--- include/Ldk/errhandlingapi.h
+++ include/Ldk/errhandlingapi.h
@@ -17,7 +17,6 @@ SetLastError(
     _In_ DWORD dwErrCode
     );
 
-#if _LDK_DEFINE_RAISE_EXCEPTION
 WINBASEAPI
 __analysis_noreturn
 VOID
@@ -28,6 +27,5 @@ RaiseException(
     _In_ DWORD nNumberOfArguments,
     _In_reads_opt_(nNumberOfArguments) CONST ULONG_PTR* lpArguments
     );
-#endif
 
 EXTERN_C_END
